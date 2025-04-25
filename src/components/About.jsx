570115f5e8e699badc063568571b63d22e70b63b
GuilhermeGongora <138256246+GuilhermeGongora@users.noreply.github.com>
2025-04-25 02:41:07 +0000

    New added sections in `Portfolio`

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,67 @@
-import React from 'react'
+import React from "react";
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { fadeIn, textVariant } from "../utils/motion";
+import { services } from "../constants";
+import { Tilt } from "react-tilt";
+import { SectionWrapper } from "../hoc";
+
+const ServiceCard = ({ index, title, icon }) => {
+  return (
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+        className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card
+        "
+      >
+        <div
+          options={{
+            max: 45,
+            scale: 1,
+            speed: 450,
+          }}
+          className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+        >
+          <img src={icon} alt={title} className="w-16 h-16 object-contain" />
+          <h3 className="text-white text-[20px] font-bold text-center">
+            {title}
+          </h3>
+        </div>
+      </motion.div>
+    </Tilt>
+  );
+};
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introdução</p>
+        <h2 className={styles.heroHeadText}>Sobre.</h2>
+      </motion.div>
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17-px] max-w-3xl leading-[30px]"
+      >
+        Sou desenvolvedor full stack com experiência em front-end e back-end,
+        utilizando tecnologias como JavaScript, TypeScript, Node.js, React,
+        Three.js e Docker. Minha missão é transformar ideias em experiências
+        digitais criativas e funcionais, sempre com foco na qualidade, inovação
+        e usabilidade. Apaixonado por programação e aprendizado contínuo, já
+        tive a oportunidade de participar de diversos projetos, ministrar
+        palestras e lecionar aulas, compartilhando conhecimento e inspirando
+        outros desenvolvedores. Acredito que a tecnologia é uma ponte entre o
+        imaginário e a realidade — e estou pronto para colaborar em projetos
+        incríveis ou ensinar o que aprendi ao longo da jornada. Vamos construir
+        algo extraordinário juntos?
+      </motion.p>
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
 
-export default About
+export default SectionWrapper(About, "about");
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,77 @@
-import React from 'react'
+import React from "react";
+import { Suspense, useEffect, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, SpotLight, useGLTF } from "@react-three/drei";
 
-const Computers = () => {
+import CanvasLoader from "../Loader";
+const Computers = ({ IsMobile }) => {
+  const computer = useGLTF("./desktop_pc/scene.gltf");
+
   return (
-    <div>Computers</div>
-  )
-}
+    <mesh>
+      <hemisphereLight intensity={1.2} groundColor="black" decay={false} />
+      <pointLight intensity={3} decay={false} />
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.55}
+        penumbra={12}
+        intensity={2}
+        castShadow
+        shadow-mapSize-width={1024}
+        shadow-mapSize-height={1024}
+        decay={false}
+      />
+      <primitive
+        object={computer.scene}
+        scale={IsMobile ? 0.7 : 0.75}
+        position={IsMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]} // Corrigido: 3 valores
+        rotation={[-0.01, -0.2, -0.1]}
+      />
+    </mesh>
+  );
+};
 
-export default Computers
+const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    // Add a listener for changes to the screen size
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+
+    // Set the initial value of the `isMobile` state variable
+    setIsMobile(mediaQuery.matches);
+
+    // Define a callback function to handle changes to the media query
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+
+    // Add the callback function as a listener for changes to the media query
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    // Remove the listener when the component is unmounted
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
+  return (
+    <Canvas
+      frameloop="demand"
+      shadows
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          enableZoom={false}
+          enablePan={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Computers IsMobile={isMobile} />
+      </Suspense>
+      <Preload all />
+    </Canvas>
+  );
+};
+export default ComputersCanvas;
